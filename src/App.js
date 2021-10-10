cad5812335c24497b19044aea8b9e52a56f4f8c2
azeezafeez <chillingloccin1@gmail.com>
2021-10-10 04:33:34 +0100

    404 page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from "./pages/Login";
-import {BrowserRouter,Route} from "react-router-dom";
+import {BrowserRouter,Route,Switch} from "react-router-dom";
 import Home from "./pages/Home";
 import Edit from "./pages/Edit";
+import NotFound from "./pages/NotFound"
+
 
 function App() {
 
@@ -11,10 +13,13 @@ function App() {
         <div className="App">
             <BrowserRouter>
                 <div className="container">
-                    <Route path="/"  exact component={Login} />
-                    <Route path="/home" component={Home}/>
-                    <Route path="/login" component={Login}/>
-                    <Route path="/edit-employee/:id" component={Edit}/>
+                    <Switch>
+                        <Route path="/"  exact component={Login} />
+                        <Route path="/home" component={Home}/>
+                        <Route path="/login" component={Login}/>
+                        <Route path="/edit-employee/:id" component={Edit}/>
+                        <Route path='*'  component={NotFound} />
+                    </Switch>
                 </div>
             </BrowserRouter>
         </div>
